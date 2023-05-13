8d36dfe560d9f7beddaddeb6142f4558b70ce8a2
Antonio <tonujet@gmail.com>
2023-05-14 01:25:51 +0200

    Add subjects mock

---
+++ client/src/data/subjects.ts
@@ -0,0 +1,90 @@
+export default [
+    {
+        name: "ВИШ МАТ",
+        desc: "lorem lorem lorem lorem lorem vvlorem lorem lorem ",
+        totalGrade: 100,
+        currGrade: 50,
+    },
+    {
+        name: "ВИШ МАТ",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce blandit urna ac tincidunt fermentum. Donec posuere eu libero id tincidunt. Curabitur ut magna arcu. Curabitur eros erat, lobortis a tincidunt ut, luctus at arcu. ",
+        totalGrade: 100,
+        currGrade: 30,
+    },
+    {
+        name: "ВИШ МАТ",
+        desc: "lorem lorem lorem lorem lorem vvlorem lorem lorem ",
+        totalGrade: 100,
+        currGrade: 20,
+    },
+    {
+        name: "Програмування",
+        desc: "lorem lorem lorem lorem lorem vvlorem lorem lorem ",
+        totalGrade: 100,
+        currGrade: 80,
+    },
+    {
+        name: "ВИШ МАТ",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce blandit urna ac tincidunt fermentum. Donec posuere eu libero id tincidunt. Curabitur ut magna arcu. Curabitur eros erat, lobortis a tincidunt ut, luctus at arcu. ",
+        totalGrade: 100,
+        currGrade: 100,
+    }, {
+        name: "ВИШ МАТ",
+        desc: "lorem lorem lorem lorem lorem vvlorem lorem lorem ",
+        totalGrade: 100,
+        currGrade: 50,
+    },
+    {
+        name: "ВИШ МАТ",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce blandit urna ac tincidunt fermentum. Donec posuere eu libero id tincidunt. Curabitur ut magna arcu. Curabitur eros erat, lobortis a tincidunt ut, luctus at arcu. ",
+        totalGrade: 100,
+        currGrade: 30,
+    },
+    {
+        name: "ВИШ МАТ",
+        desc: "lorem lorem lorem lorem lorem vvlorem lorem lorem ",
+        totalGrade: 100,
+        currGrade: 20,
+    },
+    {
+        name: "Програмування",
+        desc: "lorem lorem lorem lorem lorem vvlorem lorem lorem ",
+        totalGrade: 100,
+        currGrade: 80,
+    },
+    {
+        name: "ВИШ МАТ",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce blandit urna ac tincidunt fermentum. Donec posuere eu libero id tincidunt. Curabitur ut magna arcu. Curabitur eros erat, lobortis a tincidunt ut, luctus at arcu. ",
+        totalGrade: 100,
+        currGrade: 100,
+    },{
+        name: "ВИШ МАТ",
+        desc: "lorem lorem lorem lorem lorem vvlorem lorem lorem ",
+        totalGrade: 100,
+        currGrade: 50,
+    },
+    {
+        name: "ВИШ МАТ",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce blandit urna ac tincidunt fermentum. Donec posuere eu libero id tincidunt. Curabitur ut magna arcu. Curabitur eros erat, lobortis a tincidunt ut, luctus at arcu. ",
+        totalGrade: 100,
+        currGrade: 30,
+    },
+    {
+        name: "ВИШ МАТ",
+        desc: "lorem lorem lorem lorem lorem vvlorem lorem lorem ",
+        totalGrade: 100,
+        currGrade: 20,
+    },
+    {
+        name: "Програмування",
+        desc: "lorem lorem lorem lorem lorem vvlorem lorem lorem ",
+        totalGrade: 100,
+        currGrade: 80,
+    },
+    {
+        name: "ВИШ МАТ",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce blandit urna ac tincidunt fermentum. Donec posuere eu libero id tincidunt. Curabitur ut magna arcu. Curabitur eros erat, lobortis a tincidunt ut, luctus at arcu. ",
+        totalGrade: 100,
+        currGrade: 100,
+    }
+]
