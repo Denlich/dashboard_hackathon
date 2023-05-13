5bf1d8c18943bef5336cad258bab94089cf6381c
Yurii Sahanovskyi <yuriisahanovskyi@gmail.com>
2023-05-13 13:31:00 +0300

    Initial server commit

---
+++ server/src/test/java/com/example/Server/DashboardServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DashboardServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
