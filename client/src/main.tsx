916c01be4cab29aa59bfec60449604121e18fab9
Antonio <tonujet@gmail.com>
2023-05-14 00:53:17 +0200

    Add bootstrap dependency

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import router from "./route/router";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 import "./index.css";
 
