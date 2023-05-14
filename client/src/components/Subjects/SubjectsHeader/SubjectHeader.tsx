4950324d8f8de316807f346a3801318ebc0a8f56
Antonio <tonujet@gmail.com>
2023-05-14 03:06:53 +0200

    Add subject header component

--- client/src/components/Subjects/SubjectsHeader/SubjectHeader.tsx
+++ client/src/components/Subjects/SubjectsHeader/SubjectHeader.tsx
@@ -1,7 +1,23 @@
 import style from "./SubjectHeader.module.css";
+import {displayValue} from "@tanstack/react-query-devtools/build/lib/utils";
+import {AiOutlinePlus} from "react-icons/all";
+interface Props {
+    quantity: number,
+}
 
-const SubjectHeader = () => {
+const SubjectHeader = ({quantity}: Props) => {
+    return (
+        <>
+            <div className={style.header}>
+                <div className={style.total}>Усього: {quantity}</div>
+                <div className={style.add}>
+                        <div className={style.addName}>Додати предмет</div>
+                    <AiOutlinePlus size="20"/>
+                </div>
+            </div>
+        </>
 
+    );
 };
 
 export default SubjectHeader;
