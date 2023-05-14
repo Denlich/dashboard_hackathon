bcce97d63caf4c6acf9e4bd64f33cd707dda08fd
Antonio <tonujet@gmail.com>
2023-05-14 03:07:18 +0200

    Add stun for grade indicator

--- client/src/components/Subjects/GradeIndicator/GradeIndicator.tsx
+++ client/src/components/Subjects/GradeIndicator/GradeIndicator.tsx
@@ -1,7 +1,14 @@
 import style from "./GradeIndicator.module.css";
+interface Props{
+    totalGrade: number,
+    currGrade: number,
+}
+const GradeIndicator = ({totalGrade, currGrade}: Props) => {
+    return (
+        <div className={style.indicator}>
 
-const GradeIndicator = () => {
-
+        </div>
+    )
 };
 
 export default GradeIndicator;
