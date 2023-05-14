9d3ef2145f621bad33348aef58da96b31de44fb6
Antonio <tonujet@gmail.com>
2023-05-14 06:04:40 +0200

    Add grade indicator

--- client/src/components/Subjects/SubjectsHeader/SubjectHeader.tsx
+++ client/src/components/Subjects/SubjectsHeader/SubjectHeader.tsx
@@ -1,6 +1,5 @@
 import style from "./SubjectHeader.module.css";
 import {AiOutlinePlus} from "react-icons/all";
-import {Box, Modal, Typography} from "@mui/material";
 import {useState} from "react";
 
 interface Props {
@@ -10,7 +9,6 @@ interface Props {
 }
 
 const SubjectHeader = ({quantity, totalName, adderName}: Props) => {
-    const [isActive, setActive] = useState(false);
 
     return (
         <>
@@ -18,7 +16,6 @@ const SubjectHeader = ({quantity, totalName, adderName}: Props) => {
                 <div className={style.total}>{totalName} {quantity}</div>
                 <div
                     className={style.add}
-                    onClick={() => setActive(true)}
                 >
                     <div className={style.addName}>{adderName}</div>
                     <AiOutlinePlus size="20"/>
