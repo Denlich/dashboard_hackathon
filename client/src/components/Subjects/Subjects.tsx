be54161d4573947676a19fae80e879e3f6723968
Antonio <tonujet@gmail.com>
2023-05-14 03:07:03 +0200

    Add subjects component

--- client/src/components/Subjects/Subjects.tsx
+++ client/src/components/Subjects/Subjects.tsx
@@ -1,6 +1,27 @@
 import style from "./Subjects.module.css";
+import subjectsItems from "../../data/subjects";
+import Subject from "./Subject";
+import SubjectsHeader from "./SubjectsHeader";
+interface ISubject{
+    name: string;
+    desc: string;
+    totalGrade: number,
+    currGrade: number,
+}
 
 const Subjects = () => {
+    const subjects: ISubject[]  = subjectsItems
 
+    return (
+        <div className={style.subjects}>
+            <SubjectsHeader
+                quantity={subjects.length - 1}
+            />
+            <div className={style.subjectsList}>
+                {subjects.map(item => <Subject subject={item}/>)}
+            </div>
+        </div>
+    );
 };
 export default Subjects;
+export type {ISubject}
